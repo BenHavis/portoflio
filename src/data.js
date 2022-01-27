0013886aff6d19e669ade019c37cb9686d261efa
Ben Havis <ben.havis1@gmail.com>
2022-01-27 09:10:45 -0500

    third commit

--- src/data.js
+++ src/data.js
@@ -25,7 +25,7 @@ export const projects = [
   },
   {
     title: "React Counter",
-    subtitle: "React Hooks",
+    subtitle: "React State",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-4.gif",
